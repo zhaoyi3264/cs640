7a954304067af636b24465c2ed60f24e2e3e08f8
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-03-05 13:14:36 -0600

    add tests

--- p2/start_33.sh
+++ p2/start_33.sh
@@ -1,5 +1,8 @@
 #/bin/bash
+java -jar VirtualNetwork.jar -v s1 &> /dev/null &
+java -jar VirtualNetwork.jar -v s2 &> /dev/null &
+java -jar VirtualNetwork.jar -v s3 &> /dev/null &
 java -jar VirtualNetwork.jar -v r1 -r rtable.r1 -a arp_cache &> /dev/null &
 java -jar VirtualNetwork.jar -v r2 -r rtable.r2 -a arp_cache &> /dev/null &
 java -jar VirtualNetwork.jar -v r3 -r rtable.r3 -a arp_cache &> /dev/null &
-echo 3 routers started 
+echo 3 switches 3 routers started 
