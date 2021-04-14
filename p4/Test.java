ceacc41fe0742be0aed0c85cf1b0b1e852f8b8c0
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-04-14 16:51:30 -0500

    add test

--- p4/Test.java
+++ p4/Test.java
@@ -1,15 +1,39 @@
 import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class Test {
+
+    public static LinkedBlockingQueue<Integer> buffer = new LinkedBlockingQueue<>(10);
+
     public static void main(String[] args) {
-        InetAddress remoteAddress = null;
         try {
-            remoteAddress = InetAddress.getByName("127.0.0.1");
-        } catch (UnknownHostException e) {
+            for (int i = 0; i < 10; i++) {
+                buffer.put(i);
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
-		byte[] data = {};
-		DatagramPacket p = TCP.createPacket(remoteAddress, 8000, 0, 0, TCP.calculateFlags(1,1,0), data);
-        System.out.println(TCP.asTCP(p));
+        System.out.println(buffer);
+        new Thread(()->producer()).start();
+        new Thread(()->consumer()).start();
+    }
+
+    public static void producer() {
+        try {
+            for (int i = 0; i < 10; i++) {
+                buffer.put(i * 10);
+            }
+            System.out.println(buffer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void consumer() {
+        for (Integer i : buffer) {
+            System.out.println(i);
+        }
+        buffer.clear();
     }
 }
